3e7c3d8d38413269ce6131c6f0fca794a4f9027e
Bob Bass <bob@bbass.co>
2020-03-17 22:22:17 -0400

    setup config

---
+++ real-world-vue/src/components/EventCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Event Card</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: 'I want to test this'
+    }
+  }
+}
+</script>
+
+<style></style>
